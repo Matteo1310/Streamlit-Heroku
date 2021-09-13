5ffd732e398a124b4b0e643267828aae75ea0bef
Matteo1310 <79507093+Matteo1310@users.noreply.github.com>
2021-09-13 16:17:39 +0200

    Add files via upload

---
+++ streamlit-heroku-example-master/app.py
@@ -0,0 +1,21 @@
+import streamlit as st
+from transformers import pipeline, AutoModelForTokenClassification, AutoTokenizer
+
+# Sentiment analysis pipeline
+
+
+# Question answering pipeline, specifying the checkpoint identifier
+model = pipeline('sentiment-analysis', model='distilbert-base-uncased-finetuned-sst-2-english', tokenizer='distilbert-base-uncased-finetuned-sst-2-english')
+
+# Named entity recognition pipeline, passing in a specific model and tokenizer
+st.title('Hello World!')
+
+
+form = st.form(key='my_form')
+text = form.text_input(label='Enter some text')
+submit_button = form.form_submit_button(label='Submit')
+
+if submit_button:
+    st.subheader('Data')
+    outputs = model(text)
+    st.write({"emotion": outputs})
